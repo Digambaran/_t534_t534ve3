c12ad8a50164acdb24eeee0401062e5131eaf336
Digambaran <arjunsreekumar.1@gmail.com>
2022-06-23 14:03:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import T534ve3 from './t534ve3'
+export default function App() {
+  return (
+    <div>
+      <T534ve3/>
+    </div>
+  )
+}
---
+++ src/t534ve3.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const T534ve3 = () => {
+  return (
+   <p>Hello from t534ve3 </p>
+  )
+}
+export default T534ve3
